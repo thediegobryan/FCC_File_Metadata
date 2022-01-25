84014cfaa102a78bccb613e25135516abe93424d
thediegobryan <82959813+thediegobryan@users.noreply.github.com>
2022-01-25 14:01:04 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# FCC_File_Metadata
-
-You should provide your own project, not the example URL.
-
-You can submit a form that includes a file upload.
-
-The form file input field has the name attribute set to upfile.
-
-When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+# [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+const multer = require("multer")
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', multer().single('upfile'),(req, res)=>{
+  console.log(req.file)
+  let resObject = {};
+  resObject['name'] = req.file.originalname;
+  resObject['type'] = req.file.mimetype;
+  resObject['size'] = req.file.size;
+
+  res.json(resObject)
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
